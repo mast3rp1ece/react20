9d0b08eae7072ed01725838642236ad89f9f30e2
Taras Kotovych <t.kotovych@gmail.com>
2023-08-13 19:48:56 +0300

    set remember me

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Title } from "./styled/SignInStyled";
 import icon from '../img/padlock.png';
 import { Icon } from "./styled/SignInStyled";
@@ -15,13 +15,46 @@ import { HelpContainer } from "./styled/SignInStyled";
 export const SignIn = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-	const [rememberMe, setRememberMe] = useState('');
+	const [rememberMe, setRememberMe] = useState(false);
+
+	useEffect(() => {
+		const storedRememberMe = localStorage.getItem('rememberMe');
+		if (storedRememberMe) {
+			setRememberMe(storedRememberMe === 'true');
+		}
+  
+		if (rememberMe) {
+			const storedEmail = localStorage.getItem('rememberedEmail');
+			if (storedEmail) {
+			 setEmail(storedEmail);
+			}
+  
+			const storedPassword = localStorage.getItem('rememberedPassword');
+			if (storedPassword) {
+			 setPassword(storedPassword);
+			}
+		}
+	 }, [rememberMe]);
 
 	const handleSignIn= (e) => {
 		e.preventDefault();
 
+		if (rememberMe) {
+			localStorage.setItem('rememberedEmail', email);
+			localStorage.setItem('rememberedPassword', password);
+		}
 	}
 
+	const handleRememberMeChange = (e) => {
+		const newValue = e.target.checked;
+		setRememberMe(newValue);
+		localStorage.setItem('rememberMe', newValue.toString());
+  
+		if (!newValue) {
+			localStorage.removeItem('rememberedEmail');
+			localStorage.removeItem('rememberedPassword');
+		}
+	 };
 
 	const isEmailValid = /^[^\s@]{3,}@[^@]{2,}\.[^@]{2,}$/.test(email);
 	const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
@@ -38,7 +71,7 @@ export const SignIn = () => {
 				<SignInput className={isEmailValid ? 'valid' : (email.length === 0) ? '' : 'invalid'} type="email" placeholder="Email Address*" value={email} onChange={(e) => setEmail(e.target.value)} />
 				<SignInput className={isPasswordValid ? 'valid' : (password.length === 0) ? '0' : 'invalid'} type="password" placeholder="Password*" value={password} onChange={(e) => setPassword(e.target.value)} />
 				<div className="check-container">
-					<CheckBox checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} type="checkbox"/>
+					<CheckBox checked={rememberMe} onChange={handleRememberMeChange} type="checkbox"/>
 					<CheckInfo>Remember me</CheckInfo>
 				</div>
 				<SignButt>SIGN IN</SignButt>
